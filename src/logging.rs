933220ea178ae986762902ff547fad33940eb140
Jim Hessin <jhessin@gmail.com>
2020-03-31 14:44:19 -0600

    Rust Format
    Signed-off-by: Jim Hessin <jhessin@gmail.com>

--- src/logging.rs
+++ src/logging.rs
@@ -6,51 +6,51 @@ struct JsLog;
 struct JsNotify;
 
 impl log::Log for JsLog {
-    fn enabled(&self, _: &log::Metadata<'_>) -> bool {
-        true
+  fn enabled(&self, _: &log::Metadata<'_>) -> bool {
+    true
+  }
+  fn log(&self, record: &log::Record<'_>) {
+    let message = format!("{}", record.args());
+    js! {
+        console.log(@{message});
     }
-    fn log(&self, record: &log::Record<'_>) {
-        let message = format!("{}", record.args());
-        js! {
-            console.log(@{message});
-        }
-    }
-    fn flush(&self) {}
+  }
+  fn flush(&self) {}
 }
 impl log::Log for JsNotify {
-    fn enabled(&self, _: &log::Metadata<'_>) -> bool {
-        true
+  fn enabled(&self, _: &log::Metadata<'_>) -> bool {
+    true
+  }
+  fn log(&self, record: &log::Record<'_>) {
+    let message = format!("{}", record.args());
+    js! {
+        Game.notify(@{message});
     }
-    fn log(&self, record: &log::Record<'_>) {
-        let message = format!("{}", record.args());
-        js! {
-            Game.notify(@{message});
-        }
-    }
-    fn flush(&self) {}
+  }
+  fn flush(&self) {}
 }
 
 pub fn setup_logging(verbosity: log::LevelFilter) {
-    fern::Dispatch::new()
-        .level(verbosity)
-        .format(|out, message, record| {
-            out.finish(format_args!(
-                "({}) {}: {}",
-                record.level(),
-                record.target(),
-                message
-            ))
+  fern::Dispatch::new()
+    .level(verbosity)
+    .format(|out, message, record| {
+      out.finish(format_args!(
+        "({}) {}: {}",
+        record.level(),
+        record.target(),
+        message
+      ))
+    })
+    .chain(Box::new(JsLog) as Box<dyn log::Log>)
+    .chain(
+      fern::Dispatch::new()
+        .level(log::LevelFilter::Warn)
+        .format(|out, message, _record| {
+          let time = screeps::game::time();
+          out.finish(format_args!("[{}] {}", time, message))
         })
-        .chain(Box::new(JsLog) as Box<dyn log::Log>)
-        .chain(
-            fern::Dispatch::new()
-                .level(log::LevelFilter::Warn)
-                .format(|out, message, _record| {
-                    let time = screeps::game::time();
-                    out.finish(format_args!("[{}] {}", time, message))
-                })
-                .chain(Box::new(JsNotify) as Box<dyn log::Log>),
-        )
-        .apply()
-        .expect("expected setup_logging to only ever be called once per instance");
+        .chain(Box::new(JsNotify) as Box<dyn log::Log>),
+    )
+    .apply()
+    .expect("expected setup_logging to only ever be called once per instance");
 }
